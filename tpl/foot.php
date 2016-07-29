7777f90f0d6d988935bc0d34fb82e14301428da2
R@Me0 <r@me0.biz>
2016-07-29 11:24:05 +0300

    Removed unused js & css. Added "my.js"

--- tpl/foot.php
+++ tpl/foot.php
@@ -27,7 +27,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="/dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="/dist/js/my.js"></script>
   </body>
 </html>
