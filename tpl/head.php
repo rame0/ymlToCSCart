5bec6b3c99c044e51d17702acb205da626207059
R@Me0 <r@me0.biz>
2016-07-29 11:24:54 +0300

    Removed unused js & css. Added "my.js"

--- tpl/head.php
+++ tpl/head.php
@@ -32,9 +32,6 @@
         <!-- Bootstrap core CSS -->
         <link href="/dist/css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- Custom styles for this template -->
-        <link href="starter-template.css" rel="stylesheet">
-
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
